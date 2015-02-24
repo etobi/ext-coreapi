bca84843163e6570c3b6fed86bbbe69e259c781c
Alexander Grein <mail@typo3-webagentur.com>
2015-02-24 11:52:52 +0100

    Update ExtensionApiCommandController.php
    Add installListCommand

--- Classes/Command/ExtensionApiCommandController.php
+++ Classes/Command/ExtensionApiCommandController.php
@@ -215,6 +215,20 @@ class ExtensionApiCommandController extends CommandController {
 	}
 
 	/**
+	 * Install(activate) a comma separated list of extension.
+	 *
+	 * @param string $key The extension keys, separated by commata
+	 *
+	 * @return void
+	 */
+	public function installListCommand($keys) {
+		$keysArray = GeneralUtility::trimExplode(',', $keys, TRUE);
+		foreach ($keysArray as $key) {
+			$this->installCommand($key);
+		}
+	}
+
+	/**
 	 * UnInstall(deactivate) an extension.
 	 *
 	 * @param string $key The extension key
